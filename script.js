57fcf8aa1e91b07796a8c5b70d1df176ee76b0a1
nmariejean <mariejeajdnavales@gmail.com>
2026-05-12 01:40:15 +0800

    updated styling and sections

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ const hamburger =
       navLinks.classList.toggle("active");
     }
   );
-  
+
+//   hero section
+const text = "Web Developer & Website Support";
+const typingText = document.getElementById("typing-text");
+
+let index = 0;
+
+function typeEffect() {
+  if (index < text.length) {
+    typingText.textContent += text.charAt(index);
+    index++;
+
+    setTimeout(typeEffect, 120);
+  }
+}
+
+typeEffect();
